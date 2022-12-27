13f34cbe5828ecfeaf53c7d36be86c918f842b62
vikhyat-3 <82944741+vikhyat-3@users.noreply.github.com>
2022-12-27 09:41:02 +0530

    Update KNN.py

--- KNN.py
+++ KNN.py
@@ -13,9 +13,6 @@ iris_dataset=load_iris()
 
 #display the iris dataset
 print("\n IRIS FEATURES \ TARGET NAMES: \n ", iris_dataset.target_names)
-for i in range(len(iris_dataset.target_names)):
-    print("\n[{0}]:[{1}]".format(i,iris_dataset.target_names[i]))
-
 print("\n IRIS DATA :\n",iris_dataset["data"])
 
 #split the data into training and testing data
@@ -32,17 +29,6 @@ kn = KNeighborsClassifier(n_neighbors=5)
 kn.fit(X_train, y_train)
 
 #predicting from model
-x_new = np.array([[5, 2.9, 1, 0.2]])
-print("\n XNEW \n",x_new)
-prediction = kn.predict(x_new)
-print("\n Predicted target value: {}\n".format(prediction))
-print("\n Predicted feature name: {}\n".format(iris_dataset["target_names"][prediction]))
-
-i=1
-x= X_test[i]
-x_new = np.array([x])
-print("\n XNEW \n",x_new)
-
 for i in range(len(X_test)):
   x = X_test[i]
   x_new = np.array([x])
