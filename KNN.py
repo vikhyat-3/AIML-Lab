203a48b0e17f0228eff7db95c25add63da30c61b
vikhyat-3 <82944741+vikhyat-3@users.noreply.github.com>
2022-12-28 22:19:07 +0530

    Update KNN.py

--- KNN.py
+++ KNN.py
@@ -1,9 +1,3 @@
-# Python program to demonstrate # KNN classification algorithm # on IRISdataset
-#Write a program to implement k-Nearest Neighbour algorithm to classify the iris data set.
-#Print both correct and wrong predictions. Java/Python ML library classes can be used for
-#this problem.
-
-#import the dataset and library files
 from sklearn.datasets import load_iris
 from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
@@ -11,10 +5,8 @@ from sklearn.model_selection import train_test_split
 
 iris_dataset=load_iris()
 
-#display the iris dataset
 print("\n IRIS FEATURES \ TARGET NAMES: \n ", iris_dataset.target_names)
 
-#split the data into training and testing data
 X_train, X_test, y_train, y_test = train_test_split(iris_dataset["data"], iris_dataset["target"], random_state=0)
 print("\n Target :\n",iris_dataset["target"])
 print("\n X TRAIN \n", X_train)
@@ -22,12 +14,6 @@ print("\n X TEST \n", X_test)
 print("\n Y TRAIN \n", y_train)
 print("\n Y TEST \n", y_test)
 
-#train and fit the model
 kn = KNeighborsClassifier(n_neighbors=5)
 kn.fit(X_train, y_train)
-
-#predicting from model
-for i in range(len(X_test)):
-  prediction = kn.predict([X_test[i]])
-  print("\n Actual : {0} {1}, Predicted :{2}{3}".format(y_test[i],iris_dataset["target_names"][y_test[i]],prediction,iris_dataset["target_names"][prediction]))
 print("\n TEST SCORE[ACCURACY]: {:.2f}\n".format(kn.score(X_test, y_test)))
